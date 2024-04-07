46cd679e0c3099314c526adf84602e03f9a96201
yuccierh <yuccierh@student.21-school.ru>
2024-04-07 23:04:09 +0300

    header fix + clang

--- src/map/s21_binary_tree.h
+++ src/map/s21_binary_tree.h
@@ -2,48 +2,50 @@
 
 namespace s21 {
 
-    template <typename Key, typename T>
-    struct Node {
-        using value_type = std::pair<const Key, T>;
+template <typename Key, typename T>
+struct Node {
+  using value_type = std::pair<const Key, T>;
 
-        value_type value;
-        Node* left;
-        Node* right;
-        Node* parent;
+  value_type value;
+  Node* left;
+  Node* right;
+  Node* parent;
 
-        Node(const value_type& v)
-                : value(v), left(nullptr), right(nullptr), parent(nullptr) {}
-        size_t size() { return 1 + (left ? left->size() : 0) + (right ? right->size() : 0); }
-    };
+  Node(const value_type& v)
+      : value(v), left(nullptr), right(nullptr), parent(nullptr) {}
+  size_t size() {
+    return 1 + (left ? left->size() : 0) + (right ? right->size() : 0);
+  }
+};
 
-    template <typename Key, typename T>
-    class b_tree {
-    public:
-        using value_type = std::pair<const Key, T>;
+template <typename Key, typename T>
+class b_tree {
+ public:
+  using value_type = std::pair<const Key, T>;
 
-    private:
-        Node<Key, T>* root;
-        size_t size;
+ private:
+  Node<Key, T>* root;
+  size_t size;
 
-        void copy(Node<Key, T>* node);
-        void destroy(Node<Key, T>* node);
+  void copy(Node<Key, T>* node);
+  void destroy(Node<Key, T>* node);
 
-    public:
-        b_tree();
-        b_tree(const b_tree &other);
-        ~b_tree();
+ public:
+  b_tree();
+  b_tree(const b_tree& other);
+  ~b_tree();
 
-        b_tree& operator=(const b_tree &t) noexcept;
+  b_tree& operator=(const b_tree& t) noexcept;
 
-        Node<Key, T>* first() const;
-        Node<Key, T>* last() const;
+  Node<Key, T>* first() const;
+  Node<Key, T>* last() const;
 
-        Node<Key, T>* find_node(const Key &key) const;
-        Node<Key, T>* insert_node(value_type v);
-        void remove_node(Node<Key, T>* node);
-        void clear();
-        size_t get_size() const;
-    };
-}
+  Node<Key, T>* find_node(const Key& key) const;
+  Node<Key, T>* insert_node(value_type v);
+  void remove_node(Node<Key, T>* node);
+  void clear();
+  size_t get_size() const;
+};
+}  // namespace s21
 
 #include "s21_binary_tree.tpp"
--- src/s21_containersplus.h
+++ src/s21_containersplus.h
@@ -2,7 +2,6 @@
 #define _S21_CONTAINERSPLUS_H_
 
 #include "array/s21_array.h"
-#include "s21_containers.h"
-// #include "s21_multiset.h"
+#include "multiset/s21_multiset.h"
 
 #endif  //_S21_CONTAINERSPLUS_H_
